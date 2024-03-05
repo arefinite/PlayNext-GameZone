1d49134dff4c2c1ecabdf6486d92f5daa1d6671e
arefinite <arefinite@gmail.com>
2024-03-05 13:26:30 +0600

    footer update

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,12 @@
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <Container maxWidth="xl" sx={{pb:2 ,pt:4}}>
+      <Typography variant="body1" sx={{textAlign:'right'}}>
+        &copy; {new Date().getFullYear()} by Arefinite | All Rights Reserved.
+     </Typography>
+   </Container>
   )
 }
 export default Footer
